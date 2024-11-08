a96024f3b9c464878ec8af40b5f0c828bdecd134
Petreo <petr.kholodniuk@gmail.com>
2024-11-08 23:03:29 +0200

    Adding reverse string project to git ignore branch

---
+++ ReverseString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text");
+
+            string str = Console.ReadLine();
+
+            var newString =  ReverseString(str);
+
+            Console.WriteLine($"Your text is reverted in this: {newString}");
+            Console.ReadLine();
+        }
+         
+        
+        static string ReverseString(string str)
+        {
+            var charArray = str.ToArray();
+
+            for (int i = 0; i < str.Length /2; i++)
+            {
+                char tmp = str[i];
+                charArray[i] = charArray[charArray.Length - i - 1];
+                charArray[charArray.Length - i - 1] = tmp;
+            }
+
+            str = string.Join("", charArray);
+
+            return str;
+        }
+    }
+}
