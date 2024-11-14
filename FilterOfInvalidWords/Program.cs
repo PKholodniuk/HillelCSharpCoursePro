838f43618358e07bbaeb2310d2cbdc6d9cafe2ef
Petreo <petr.kholodniuk@gmail.com>
2024-11-14 21:08:20 +0200

    Adding new task from lesson two and changing array

---
+++ FilterOfInvalidWords/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FilterOfInvalidWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Mother Fuck, where did you piss with your penis?";
+
+            string[] array = { "bitch", "penis", "fuck", "piss"  };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                {
+                    text = Regex.Replace(text, Regex.Escape(array[i]), new string('*', array.Length), RegexOptions.IgnoreCase);
+                }
+            }
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
